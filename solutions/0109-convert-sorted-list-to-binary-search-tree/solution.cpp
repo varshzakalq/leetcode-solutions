45288ba4f9ab793786fd22ba170dea1d4975a9b1
varshzakalq <114610090+varshzakalq@users.noreply.github.com>
2026-06-13 18:08:36 +0000

    Sync LeetCode submission - Convert Sorted List to Binary Search Tree - Runtime - 7 ms (27.52%), Memory - 32.9 MB (82.99%)

---
+++ solutions/0109-convert-sorted-list-to-binary-search-tree/solution.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* findMid(ListNode*root){
+        ListNode* slow = root;
+        ListNode* prev = nullptr;
+        ListNode* fast = root;
+        while(fast != NULL&& fast->next != NULL){
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        if(prev!= NULL){
+            prev->next = NULL;
+        }
+        return slow;
+    }
+    TreeNode* sortedListToBST(ListNode* head) {
+            if(head == NULL)return NULL;
+            if(head->next ==NULL )return new TreeNode(head->val);
+            ListNode* mid = findMid(head);
+            TreeNode* t = new TreeNode;
+            t->val = mid->val;
+            t->left = sortedListToBST(head);
+            t->right = sortedListToBST(mid->next);
+
+            return t;
+
+    }
+};
