661a797a396f703c3ad494b32cd5cb5049672125
varshzakalq <114610090+varshzakalq@users.noreply.github.com>
2026-07-04 07:09:18 +0000

    Sync LeetCode submission - Spiral Matrix II - Runtime - 0 ms (100.00%), Memory - 9.5 MB (43.27%)

---
+++ solutions/0059-spiral-matrix-ii/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> matrix (n, vector<int>(n,0));
+int left = 0, right = n - 1;
+int top = 0, bottom = n - 1;
+int num = 1;
+
+while (left <= right && top <= bottom) {
+
+    for (int j = left; j <= right; j++)
+        matrix[top][j] = num++;
+
+    top++;
+
+    for (int i = top; i <= bottom; i++)
+        matrix[i][right] = num++;
+
+    right--;
+
+    if (top <= bottom) {
+        for (int j = right; j >= left; j--)
+            matrix[bottom][j] = num++;
+        bottom--;
+    }
+
+    if (left <= right) {
+        for (int i = bottom; i >= top; i--)
+            matrix[i][left] = num++;
+        left++;
+    }
+}
+return matrix;
+    }
+};
