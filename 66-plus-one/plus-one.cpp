0372a52a168801e779e50cf4dc4f047717d9fa19
varshzakalq <114610090+varshzakalq@users.noreply.github.com>
2026-07-10 22:38:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (62.05%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry =1;
+        for(int i =digits.size()-1;i>=0;i--){
+            int num= digits[i]+carry;
+            digits[i]= num%10;
+            carry = num/10;
+        }
+        if(carry ==1){
+            digits.insert(digits.begin(),1);
+        }
+        return digits;
+    }
+};
