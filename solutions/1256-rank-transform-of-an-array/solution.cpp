94e3ea5cf0f932720bfc8537a830543381801758
varshzakalq <114610090+varshzakalq@users.noreply.github.com>
2026-07-12 15:31:27 +0000

    Sync LeetCode submission - Rank Transform of an Array - Runtime - 39 ms (47.30%), Memory - 43.8 MB (67.10%)

---
+++ solutions/1256-rank-transform-of-an-array/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> temp = arr;
+        sort(temp.begin(),temp.end());
+        unordered_map <int,int> mp;
+        int count = 1;
+        for(int i =0;i<arr.size();i++){
+            if(mp.find(temp[i]) == mp.end()){
+                 mp[temp[i]] =count++;
+            }               
+        }
+        for(int i =0;i<arr.size();i++){
+            arr[i] = mp[arr[i]];
+        }
+        return arr;
+    }
+};
