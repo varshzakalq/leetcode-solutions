938cfbd1f06f30888e9f27c7e4225acdfeffbc53
varshzakalq <114610090+varshzakalq@users.noreply.github.com>
2026-07-12 20:59:02 +0530

    Time: 139 ms (5.25%) | Memory: 43.8 MB (71.43%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> temp = arr;
+        sort(temp.begin(),temp.end());
+        unordered_map <int,int> mp;
+        int count = 1;
+        for(int i =0;i<arr.size();i++){
+            if(!mp[temp[i]]){
+                 mp[temp[i]] =count++;
+                 cout<<i;
+            }               
+        }
+        for(int i =0;i<arr.size();i++){
+            arr[i] = mp[arr[i]];
+        }
+        return arr;
+    }
+};
