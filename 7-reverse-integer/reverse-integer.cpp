f21572fc4a6ab90b852df7a0784d4040a855642d
varshzakalq <114610090+varshzakalq@users.noreply.github.com>
2026-07-14 21:14:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.91%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     int reverse(int x) {
-        string rev;
-        if(x<0){
-            if(x ==numeric_limits<int>::min())
+        int rev =0;
+        while(x !=0){
+            int digit = x%10;
+            x /= 10;
+            if(rev>INT_MAX/10 || (rev == INT_MAX/10 && digit > 7)){
                 return 0;
-
-            rev = to_string(-x);
-            ::reverse(rev.begin(),rev.end());
-            rev = "-" +rev;
-        }
-           
-        else{
-            rev = to_string(x);
-            ::reverse(rev.begin(),rev.end());
-        }
-        try{
-            int a = stoi(rev);
-            return a;
-        }
-        catch(const std::out_of_range& e){
-            return 0;
-        }
-
+            }
+            if(rev <INT_MIN /10||( rev == INT_MIN/10 && digit < -8)){
+                return 0;
+            }
+            rev = rev *10 +digit;
             
+        }
+        return rev;
     }
 };
