3160146a7b41684735361089164241b291a9bb70
varshzakalq <114610090+varshzakalq@users.noreply.github.com>
2026-07-28 10:35:27 +0000

    Sync LeetCode submission - Remove Element - Runtime - 0 ms (100.00%), Memory - 11.6 MB (85.20%)

---
+++ 0027-remove-element/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(std::vector<int>& nums, int val) {
+        int k = 0; // Pointer to place the next element not equal to val
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        
+        return k;
+    }
+};
